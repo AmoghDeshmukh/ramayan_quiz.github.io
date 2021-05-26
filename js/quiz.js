cf1ae1448cb4f130e0d5dde2d572e6c6654c8906
AmoghDeshmukh <amoghndeshmukh@gmail.com>
2021-05-26 19:33:04 +0530

    online quiz

---
+++ js/quiz.js
@@ -0,0 +1,101 @@
+let quiz = [
+    {
+        question: "Lord Rama was belonging to which generation?",
+        option: [
+            "Surya Vansh",
+            "Yadu Vansh",
+            "Chandra Vansh",
+            "Raghu Vansh",
+        ],
+        answer: 1,
+    },
+    {
+        question: "Who was Lord Rama's Guru?",
+        option: [
+            "Guru Shukracharya",
+            "Guru Vishwamitra",
+            "Guru Vaishitha ",
+            "Guru Dronacharya",
+        ],
+        answer: 2,
+    },
+    {
+         question: "Who was the mother of Goddess Sita?",
+        option: [
+            "Tara",
+            "Sunayana",
+            "Simhika",
+            "Sulochana",
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            "What was the name of Lord Shiva's Bow which was broken by Lord Rama?",
+        option: [
+            "Gandeev",
+            "Koumodaki",
+            "Vijaya",
+            "Pinaka",
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            "Who was Jatayu's brother?",
+        option: [
+            "Aruna",
+            "Mareecha",
+            "Sampathi",
+            "Jambavanta",
+        ],
+        answer: 3,
+    },
+    {
+        question: "What was the name of Sugreeva's Wife?",
+        option: [
+            "Tara", 
+            "Roma", 
+            "Mandodari", 
+            "Jahnavi"],
+        answer: 2,
+    },
+    {
+        question: "Who was the son of Hanumana?",
+        option: [
+            "Sasdhwaja",
+            "Kumarveera",
+            "Makardhwaja",
+            "Sasula",
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            "What was the name of Ravana's mother?",
+        option: [
+            "Mandodari", 
+            "Kaikesi", 
+            "Trishera", 
+            "Kumbhini"],
+        answer: 2,
+    },
+    {
+        question: "Choose the correct relation between Hidimba-Ravana?",
+        option: [
+            "Sister-in-law  - Father",
+             "Sister-Brother", 
+             "Cousins", 
+             "No-relation"],
+        answer: 4,
+    },
+    {
+        question: "How many days did Lord Rama and Ravana fight a fierce battle?",
+        option: [
+            "15", 
+            "30", 
+            "7", 
+            "45"],
+        answer: 3,
+    },
+];
